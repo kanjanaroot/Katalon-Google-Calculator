883e010bc1fb3d7594515ea5bb1ad0099c24b02d
kanjanaroot <Kanjanaroot_K@cmu.ac.th>
2022-02-04 01:22:04 +0700

    THE_One
    Ez style

---
+++ Object Repository/Page_calculator -  Google/number_input.rs
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>number_input</name>
+   <tag></tag>
+   <elementGuidId>a2cfa484-0ac7-46c2-8755-8baebab583f2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.z7BZJb.XSNERd</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='rso']/div/div/div/div/div/div/div/div[2]/div[2]/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>jsname</name>
+      <type>Main</type>
+      <value>zLiRgc</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>z7BZJb XSNERd</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>  11 (function(){
+var a=document.getElementById(&quot;cwos&quot;),b;var c=parseFloat(a.innerText||a.textContent),d=c.toString();if(12>=d.replace(/^-/,&quot;&quot;).replace(/\./,&quot;&quot;).length)b=d;else if(d=c.toPrecision(12),-1!==d.indexOf(&quot;e&quot;)){var e,f,g,h=null!=(g=null==(e=d.match(/e.*$/))?void 0:null==(f=e[0])?void 0:f.length)?g:0;b=c.toPrecision(12-h-(&quot;0&quot;===d[0]?1:0)).replace(/\.?0*e/,&quot;e&quot;)}else{var k=d.match(/(^-|\.)/g),l=d.substr(0,12+(k?k.length:0));b=-1!==l.indexOf(&quot;.&quot;)?l.replace(/\.?0*$/,&quot;&quot;):l}a.textContent=b;}).call(this); </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;rso&quot;)/div[@class=&quot;ULSxyf&quot;]/div[@class=&quot;KIy09e obcontainer wDYxhc NFQFxe&quot;]/div[1]/div[@class=&quot;card-section&quot;]/div[1]/div[@class=&quot;tyYmIf&quot;]/div[@class=&quot;BRpYC&quot;]/div[@class=&quot;TIGsTb lqyc1 d0jRmd&quot;]/div[@class=&quot;fB3vD&quot;]/div[@class=&quot;jlkklc&quot;]/div[@class=&quot;z7BZJb XSNERd&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='rso']/div/div/div/div/div/div/div/div[2]/div[2]/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Ans = 0'])[1]/following::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='การคำนวณและผลลัพธ์ของคุณจะปรากฏที่นี่ เพื่อให้คุณนำไปใช้ซ้ำได้'])[1]/following::div[9]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Rad'])[1]/preceding::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Deg'])[1]/preceding::div[5]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '  11 (function(){
+var a=document.getElementById(&quot;cwos&quot;),b;var c=parseFloat(a.innerText||a.textContent),d=c.toString();if(12>=d.replace(/^-/,&quot;&quot;).replace(/\./,&quot;&quot;).length)b=d;else if(d=c.toPrecision(12),-1!==d.indexOf(&quot;e&quot;)){var e,f,g,h=null!=(g=null==(e=d.match(/e.*$/))?void 0:null==(f=e[0])?void 0:f.length)?g:0;b=c.toPrecision(12-h-(&quot;0&quot;===d[0]?1:0)).replace(/\.?0*e/,&quot;e&quot;)}else{var k=d.match(/(^-|\.)/g),l=d.substr(0,12+(k?k.length:0));b=-1!==l.indexOf(&quot;.&quot;)?l.replace(/\.?0*$/,&quot;&quot;):l}a.textContent=b;}).call(this); ' or . = '  11 (function(){
+var a=document.getElementById(&quot;cwos&quot;),b;var c=parseFloat(a.innerText||a.textContent),d=c.toString();if(12>=d.replace(/^-/,&quot;&quot;).replace(/\./,&quot;&quot;).length)b=d;else if(d=c.toPrecision(12),-1!==d.indexOf(&quot;e&quot;)){var e,f,g,h=null!=(g=null==(e=d.match(/e.*$/))?void 0:null==(f=e[0])?void 0:f.length)?g:0;b=c.toPrecision(12-h-(&quot;0&quot;===d[0]?1:0)).replace(/\.?0*e/,&quot;e&quot;)}else{var k=d.match(/(^-|\.)/g),l=d.substr(0,12+(k?k.length:0));b=-1!==l.indexOf(&quot;.&quot;)?l.replace(/\.?0*$/,&quot;&quot;):l}a.textContent=b;}).call(this); ')]</value>
+   </webElementXpaths>
+</WebElementEntity>
